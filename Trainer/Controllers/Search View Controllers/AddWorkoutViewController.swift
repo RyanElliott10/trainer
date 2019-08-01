e0dca802e957902e0a100807df49d1b605da2e4f
Ryan Elliott <ryelliot@calpoly.edu>
2019-07-31 22:36:50 -0700

    Added gesture recognizer

--- Trainer/Controllers/Search View Controllers/AddWorkoutViewController.swift
+++ Trainer/Controllers/Search View Controllers/AddWorkoutViewController.swift
@@ -25,6 +25,10 @@ class AddWorkoutViewController: UIViewController {
         
         view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
         view.isOpaque = false
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(dismissSelf))
+        gesture.delegate = self
+        view.addGestureRecognizer(gesture)
         setupViews()
     }
     
@@ -36,6 +40,20 @@ class AddWorkoutViewController: UIViewController {
             viewContainer.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             viewContainer.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         ])
+    }
+    
+    @objc private func dismissSelf() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // TODO: - Add gesture recognizer to view. and on press display an alert asking the user if they want to dismiss their progress
+    
+}
+
+extension AddWorkoutViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return touch.view == gestureRecognizer.view
     }
     
 }
